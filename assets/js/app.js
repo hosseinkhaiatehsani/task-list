a1a6e89928ad8a7d8a77888755ac282c71c37698
hosseinkhaiatehsani <hos.kh93@gmail.com>
2023-05-22 18:58:46 +0330

    initialize to do list

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+// import { TEST } from "./test.js";
+
+const app = document.querySelector("#app");
+
+// console.log(TEST);
+
+document.addEventListener("DOMContentLoaded", function(){
+
+});
+
+
+function createToDoInput(){
+    let inputContainer = document.createElement("div");
+    let input = document.createElement("input");
+
+    
+}
